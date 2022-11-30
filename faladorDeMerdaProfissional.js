637b4f9889c18c8e57e8237c94714de8472a620a
mateusredfield <mateuscampari@gmail.com>
2022-11-30 15:26:00 -0300

    meu carallho ta fodda, pulta q pariu

---
+++ faladorDeMerdaProfissional.js
@@ -0,0 +1,6 @@
+function Naosei() {
+
+    return <h2>mol calor</h2>
+}
+
+ReactDOM.render( <Naosei />, document.getElementById("rg") )
--- fodasse.js
+++
@@ -1,1 +0,0 @@
-ReactDOM.render(<h2>Uau que linguagem dificil</h2>, document.getElementById("rg"))
